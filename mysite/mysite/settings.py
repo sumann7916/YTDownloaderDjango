72fe56ed8be7269bfb35da59e51331d111d92f6a
sumann7916 <62978690+sumann7916@users.noreply.github.com>
2020-05-06 16:37:56 +0545

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*o&uhj)wm%-5mafdlgax4swmf+rpx80%@=tshhc-n0j%1&=#)m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.254.1', '192.168.254.3']
+ALLOWED_HOSTS = []
 
 
 # Application definition
